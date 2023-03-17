b36a6dd2eb3d0836b4711af5c863b48debe59321
Patrick Thiel <patrickthiel@outlook.de>
2023-03-17 14:52:03 +0100

    Added new theme, styling, features for adding data and allow enter for searching

--- shiny/server.R
+++ shiny/server.R
@@ -1,27 +1,22 @@
+#--------------------------------------------------
+# file location
+# NOTE: Is there a way to make it not rely on the file being existent on my
+# local drive?
+file_loc <- "G:/work/horizon_database/horizon_app/shiny/www"
+
 # define server logic
 server <- function(input, output) {
     #--------------------------------------------------
     # Search feature
   
     # read data from Excel
-    database <- readxl::read_excel("G:/work/horizon_database/data/database.xlsx")
-    
-    # Show complete table if switch "Show all data" is ON
-    # complete_table <- reactive(
-    #     {
-    #         if(input$show_all == TRUE){
-    #             database
-    #         }
-    #     }
-    # )
+    database <- readxl::read_excel(file.path(file_loc, "database.xlsx"))
     
     # Filter table based on user input
     filtered <- eventReactive(input$submit,
         {
             if(input$search_query != "") {
-                # dplyr::filter(stringr::str_detect(tolower(dateset), tolower(input$search_query)) == TRUE)
                 stringr::str_detect(tolower(database$dataset), tolower(input$search_query))
-                # dplyr::filter(str_detect(tolower(key_words), tolower(input$search_query) == TRUE))
             } else {
                 TRUE
             }
@@ -30,7 +25,62 @@ server <- function(input, output) {
     
     # Render table based on filter input
     output$table_filtered <- renderTable(database[filtered(), ])
-    # output$table_complete <- renderTable(complete_table())
+
+    #--------------------------------------------------
+    # Adding new data sets
+    
+    # set up a popup window to enter new information
+    # pops up then "add" button is clicked
+    added <- observeEvent(
+        input$add,
+        {
+            showModal(
+                modalDialog(
+                    tags$h2("Add metadata"),
+                    # Define meta data fields
+                    textInput(inputId = "dataset_name", label = "", placeholder = "Name of the data set"),
+                    textInput(inputId = "keywords", label = "", placeholder = "Key words"),
+                    textInput(inputId = "time_range", label = "", placeholder = "Time range"),
+                    textInput(inputId = "provider", label = "", placeholder = "Data provider"),
+                    # Buttons to submit or cancel input
+                    footer = tagList(
+                        actionButton(inputId = "ok", label = "Submit"),
+                        modalButton("Cancel")
+                    )
+                )
+            )
+        }
+    )
+    
+    # grab entered information and store it into summary table
+    table <- reactive({
+        data.frame(
+            `Data set` = input$dataset_name,
+            Keywords = input$keywords,
+            `Time Range` = input$time_range,
+            Provider = input$provider,
+            check.names = FALSE # to allow for column names with space
+        )
+    })
+    
+    # pop-up window to show the user what was entered
+    # wait for confirmation
+    observeEvent(
+        input$ok,
+        {
+            showModal(
+                modalDialog(
+                    tags$h2("Do you want to add the following data set?"),
+                    renderTable(table()),
+                    footer = tagList(
+                        actionButton(inputId = "yes", label = "Yes"),
+                        modalButton("No")
+                    )
+                    
+                )
+            )
+        }
+    )
     
     
 
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,62 +1,124 @@
-ui <- navbarPage(
+#--------------------------------------------------
+# load libraries
+
+library(shiny)
+library(bslib)
+library(particlesjs)
+library(shinyjs)
+
+#--------------------------------------------------
+# JS code for allowing to hit ENTER for inputs
+
+jscode <- '
+$(function() {
+  var $els = $("[data-proxy-click]");
+  $.each(
+    $els,
+    function(idx, el) {
+      var $el = $(el);
+      var $proxy = $("#" + $el.data("proxyClick"));
+      $el.keydown(function (e) {
+        if (e.keyCode == 13) {
+          $proxy.click();
+        }
+      });
+    }
+  );
+});
+'
+
+#--------------------------------------------------
+# file locations
+# NOTE: Is there a way to make it not rely on the file being existent on my
+# local drive?
+file_loc <- "G:/work/horizon_database/horizon_app/shiny/www"
+
+
+#--------------------------------------------------
+# define user interface
+
+ui <- fluidPage(
+    tags$head(tags$script(HTML(jscode))),
+    # add overall styling
+    tags$head(
+        tags$link(rel = "stylesheet", type = "text/css", href = file.path(file_loc, "mystyling.css"))
+    ),
+    # add tab title
     title = "Database for Environmental Data",
     fluid = TRUE,
-    theme = shinythemes::shinytheme("paper"),
-    # tabPanel(
-    #     "Home"
-    #     # shinyjs::useShinyjs(),
-    #     # div(
-    #     #     class = "container",
-    #     #     style = "min-height:90vh;"
-    #     # )
-    # ),
+    theme = bslib::bs_theme(bootswatch = "quartz"),
+    id = "tabs",
     tabPanel(
-        "Data base",
-        tabsetPanel(
-            type = "tabs",
-            #--------------------------------------------------
-            # Search feature
-            tabPanel(
-                "Search",
-                fluidRow(
-                    column(
-                        6,
-                        textInput(inputId = "search_query", "", width = "100%", placeholder = "Enter you search here")
-                    ),
-                    column(
-                        1,
-                        br(),
-                        actionButton(inputId = "submit", label = "Search")
-                    ),
-                    # column(
-                    #     3,
-                    #     br(),
-                    #     shinyWidgets::materialSwitch(inputId = "show_all", label = "Show all data", value = TRUE, status = "success")
-                    # )
+        "Home",
+        shinyjs::useShinyjs(),
+        div(
+            class = "container",
+            style = "min-height:90%;",
+            # define background picture and main title
+            div(
+                style = "width: 100%; height: 300px; position: relative;z-index:-9;",
+                div(
+                    id = "particles-target",
+                    style = "position: absolute; top: 0; bottom: 0; right: 0; left: 0;"
                 ),
-                # fluidRow(
-                #     tableOutput("table_complete")
-                # ),
-                fluidRow(
-                    tableOutput("table_filtered")
+                div(
+                    p(
+                        "Database for Environmental Data",
+                        align = "center"
+                    ),
+                    style = "padding-top:90px; font-size:60px",
                 )
             ),
-            #--------------------------------------------------
-            # Adding new data sets
-            tabPanel(
-                "Add new data set",
-                fluidRow(
-                    column(
-                        5,
-                        textInput(
-                            inputId = "text",
-                            label = "TESTS",
-                            width = "100%",
-                            placeholder = "Test"
+            # particles background picture
+            particlesjs::particles(file.path(file_loc, "particles.json"), target_id = "particles-target", element_id =  "particles"),
+            # define features
+            tabsetPanel(
+                type = "hidden",
+                #--------------------------------------------------
+                # Search feature
+                tabPanel(
+                    "Search",
+                    fluidRow(
+                        column(
+                            2
+                        ),
+                        column(
+                            6,
+                            tagAppendAttributes(
+                                textInput(inputId = "search_query", "", width = "100%", placeholder = "Enter you search here"),
+                                `data-proxy-click` = "submit"
+                            )
+                        ),
+                        column(
+                            1,
+                            br(),
+                            actionButton(inputId = "submit", label = "Search")
+                        ),
+                        column(
+                            3,
+                            br(),
+                            actionButton(inputId = "add", label = "Add data")
+                        )
+                    ),
+                    fluidRow(
+                        column(
+                            2
+                        ),
+                        column(
+                            6,
+                            tableOutput(outputId = "table_filtered")
                         )
                     )
+                    # fluidRow(
+                    #     column(
+                    #         12,
+                    #         textOutput(outputId = "text")
+                    #     )
+                    # )
                 )
-            )
+            ),
+            hr(),
+            print("DISCLAIMER")
         )
     )
 )
